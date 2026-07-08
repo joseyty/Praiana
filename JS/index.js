80d2b6966aa3045262386c597038d408e2f65141
Igor jose <lmelo7262@gmail.com>
2026-07-08 17:47:59 -0300

    adicionando card,animação...

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,20 @@
 function tocarMusica(){
 
     document.getElementById("musica").play();
+    document.getElementById("musica2").play();
+      document.getElementById("musica3").play();
+        document.getElementById("musica4").play();
+          document.getElementById("musica5").play();
+
+
+
+
+
+
+
+
+
+
 }
+
+
