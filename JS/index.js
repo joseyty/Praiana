d89e314d115c855d8433da803c6a9838a2bd366a
Igor jose <lmelo7262@gmail.com>
2026-07-18 17:37:00 -0300

    musica

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,33 @@
-function tocarMusica(){
+
+
+const audioP = Document.getElementById("musica");
+
+
+
+function backMusic(){
+  audioP.currentTime= 0;
+}
+
+
+
+function playMusic() {
+    if (musica.paused) {
+        musica.play();
+        botao.textContent = "⏸️ Pausar";
+    } else {
+        musica.pause();
+        botao.textContent = "▶️ Tocar";
+    }
+}
+
+
+function nextMusic(){
 
     document.getElementById("musica").play();
     document.getElementById("musica2").play();
       document.getElementById("musica3").play();
         document.getElementById("musica4").play();
           document.getElementById("musica5").play();
-
-
-
-
-
-
-
-
+          
         }
 
